9b3b54ec7b91ac10d2cbb48cb1eba35f48e85e92
Son1824 <163498771+Son1824@users.noreply.github.com>
2024-10-25 18:43:09 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,18 +2,18 @@ package QueueDemo.LinkedListBasedQueue;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedListBasedQueue queue = new LinkedListBasedQueue();
+        LinkedListBasedQueue linkedListQueue = new LinkedListBasedQueue();
         System.out.println("Linked List-Based Queue:");
 
-        queue.enqueue(4);
-        queue.enqueue(5);
-        queue.enqueue(6);
+        linkedListQueue.enqueue(4);
+        linkedListQueue.enqueue(5);
+        linkedListQueue.enqueue(6);
 
-        queue.displayQueue();
+        linkedListQueue.displayQueue();
 
-        System.out.println("Dequeued element: " + queue.dequeue());
-        queue.displayQueue();
+        System.out.println("Dequeued element: " + linkedListQueue.dequeue());
+        linkedListQueue.displayQueue();
 
-        System.out.println("Is queue empty? " + queue.isEmpty());
+        System.out.println("Is queue empty? " + linkedListQueue.isEmpty());
     }
 }
